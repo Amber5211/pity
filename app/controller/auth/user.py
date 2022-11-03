a6021c0e886a3082b075145ef3542c12e6044fd0
Amber5211 <747852635@qq.com>
2022-11-03 12:16:58 +0800

    新增登录接口

--- app/controller/auth/user.py
+++ app/controller/auth/user.py
@@ -1,6 +1,8 @@
 from flask import Blueprint,request
 from flask import jsonify
 from app.controller.auth.UserDao import UserDao
+from app.handler.factory import ResponseFactory
+from app.middleware.Jwt import UserToken
 
 auth=Blueprint('auth',__name__,url_prefix='/auth')
 
@@ -24,3 +26,21 @@ def register():
         return jsonify(dict(code=110,msg=err))
 
     return jsonify(dict(code=0,msg='注册成功'))
+
+# 登录
+@auth.route('/login',methods=['POST'])
+def login():
+    data=request.get_json()
+    username,password=data.get('username'),data.get('password')
+    if not username or not password:
+        return jsonify(dict(code=101,msg='用户名或密码不存在'))
+    user,err=UserDao.login(username,password)
+    if err is not None:
+        return jsonify(dict(code=110,msg=err))
+    # usermodel对象转换成dict
+    user=ResponseFactory.model_to_dict(user,'password')
+    # 获取token
+    token=UserToken.get_token(user)
+
+    return jsonify(dict(code=0,msg='登录成功',data=dict(token=token,user=user)))
+    
